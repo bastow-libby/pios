f322389e74f0011a30af1f68197d8fc91129387f
Elizabeth Bastow <ebastow@luc.edu>
2024-09-11 21:21:52 -0500

    Completed linked list functions

--- src/list.c
+++ src/list.c
@@ -1,17 +1,25 @@
+#include "list.h"
+#include <stddef.h>
 
 struct list_element c = {NULL, 0};
 struct list_element b = {&c, 0};
 struct list_element a = {&b, 0};
+struct list_element *head = &a;
+
 
 //write the list_add() function that adds elements to a linked list
-void list_add(struct list_element *list_head, struct list_element *new element){
-	list_head->next = &new;
-	return 0;
+void list_add(struct list_element *list_head, struct list_element *new_element){
+	new_element->next = list_head->next; //new element's next becomes the 
+					     //same as the head's next (null)
+	list_head->next = new_element; //old head now points to new_element
 }
 
 
 //write the list_remove() function that removes elements from a linked list
-void list_remove(struct list_element *element){
-
-	return 0;
+void list_remove(struct list_element *list_head, struct list_element *element){
+	struct list_element *current = list_head;
+	while (current->next != NULL && current->next != element) {
+		current = current->next;
+	}
+	current->next = element->next;
 }
--- src/list.h
+++ src/list.h
@@ -1,8 +1,17 @@
+#ifndef LIST_H
+#define LIST_H
+
+
 //general purpose structure for a linked list
 //
 struct list_element {
 	struct list_element *next;
 	int data;	
 };
+
+void list_add(struct list_element *list_head, struct list_element *new_element);void list_remove(struct list_element *list_head, struct list_element *element);
+
+
+#endif 
 
 

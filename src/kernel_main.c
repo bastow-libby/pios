1163b674da7a0cb38b556ad6fc0c3e815f310fb4
Elizabeth Bastow <ebastow@luc.edu>
2024-09-11 20:42:43 -0500

    Clearing bss and reading time done, still working on linked lists

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,10 +1,28 @@
 
+unsigned long get_timer_count(){
+        unsigned long *timer_count_register = 0x3f003004;
+        return *timer_count_register;
+}
 
+char glbl[128];
 
 void kernel_main() {
+    unsigned long *time = get_timer_count();
+    
 
-    //Adding a comment to test git and push
+    //initialize variables, bring in external variables
+    char *bssstart, *bssend;
+    extern char __bss_start, __bss_end;
+    //set variables equal to values found in linkerscript
+    bssstart = &__bss_start;
+    bssend = &__bss_end;
+    //edit for testing 
 
+    //loop to iterate through bss, setting each value to zero
+    char *i;
+    for (i = bssstart; i < bssend; i++) {
+   	*i = 0;	
+    }
     while(1){
     }
 }

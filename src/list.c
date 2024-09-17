f7fca1e42a579445e75a243553d59bcc49b965ae
Elizabeth Bastow <ebastow@luc.edu>
2024-09-17 14:29:18 -0500

    updated link list code

--- src/list.c
+++ src/list.c
@@ -8,10 +8,10 @@ struct list_element *head = &a;
 
 
 //write the list_add() function that adds elements to a linked list
-void list_add(struct list_element *list_head, struct list_element *new_element){
-	new_element->next = list_head->next; //new element's next becomes the 
+void list_add(struct list_element **list_head, struct list_element *new_element){
+	new_element->next = (struct list_element*)list_head; //new element's next becomes the 
 					     //same as the head's next (null)
-	list_head->next = new_element; //old head now points to new_element
+	*list_head = new_element; //head now points to new_element
 }
 
 
--- src/list.h
+++ src/list.h
@@ -9,7 +9,7 @@ struct list_element {
 	int data;	
 };
 
-void list_add(struct list_element *list_head, struct list_element *new_element);void list_remove(struct list_element *list_head, struct list_element *element);
+void list_add(struct list_element **list_head, struct list_element *new_element);void list_remove(struct list_element *list_head, struct list_element *element);
 
 
 #endif 

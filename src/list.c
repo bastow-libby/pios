b4d0879ed24302fa83fe9292730d4aeaf4c02783
Elizabeth Bastow <ebastow@luc.edu>
2024-10-09 21:27:37 -0500

    Not tested, but at least complete code

--- src/list.c
+++ src/list.c
@@ -7,14 +7,16 @@ void list_add(struct list_element **list_head, struct list_element *new_element)
 	new_element->next = (struct list_element*)list_head; //new element's next becomes the 
 					     //same as the head's next (null)
 	*list_head = new_element; //head now points to new_element
+	new_element->prev = list_head;
 }
 
 
 //write the list_remove() function that removes elements from a linked list
 void list_remove(struct list_element *list_head, struct list_element *element){
 	struct list_element *current = list_head;
-	while (current->next != NULL && current->next != element) {
+	while (current != NULL && current != element) {
 		current = current->next;
 	}
-	current->next = element->next;
+	current->prev->next = current->next;
+	current->next->prev = current->prev;
 }
--- src/list.h
+++ src/list.h
@@ -6,6 +6,7 @@
 //
 struct list_element {
 	struct list_element *next;
+	struct list_element *prev;
 	int data;	
 };
 

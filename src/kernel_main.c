46f72d3db40afda91d0798929386479add332991
Elizabeth Bastow <ebastow@luc.edu>
2024-10-15 12:36:24 -0500

    MMU hw

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -2,6 +2,8 @@
 #include <stddef.h>
 #include "rprintf.h"
 #include "serial.h"
+#include "mmu.h"
+#include "page.h"
 unsigned long get_timer_count(){
         unsigned long *timer_count_register = 0x3f003004;
         return *timer_count_register;
@@ -19,8 +21,9 @@ int getEL() {
 char glbl[128];
 
 void kernel_main() {
-    //unsigned long time = get_timer_count();
-    //milisec_delay();
+    mapPages(0x0000, 0x0000);
+    //Call load page table
+
     esp_printf(putc, "Current Execution Level is %d\r\n", getEL());
     //initialize variables, bring in external variables
     char *bssstart, *bssend;

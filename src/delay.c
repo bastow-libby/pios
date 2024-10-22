0f96364f3bedd819c02f28100f78f87d1c79738c
Elizabeth Bastow <ebastow@luc.edu>
2024-10-22 17:25:57 -0500

    updated delay and wait tick

--- src/delay.c
+++ src/delay.c
@@ -7,7 +7,10 @@ void wait_msec(int number) {
 
 }
 void wait_cycles(int time) {
-	for(int i = 0; i < time; i++) {
-		wait_msec(1);
-	}
+	unsigned long start_time = get_timer_count();
+        unsigned long time_passed = start_time - get_timer_count();
+        while(time_passed <= time){
+                time_passed = start_time - get_timer_count();
+        }
+
 }
